cca238f58530ad63f3ae8a4d083aeb532d63f18d
Wa-dot <842G1thub@$52>
2023-11-06 08:21:39 +0100

    try1 fix  and this error

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 import Vue from 'vue'
+import { ComponentCustomProperties } from 'vue';
+import { VueI18n } from 'vue-i18n';
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $t: VueI18n['t'];
+        $i18n: VueI18n;
+    }
+}
